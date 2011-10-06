e414c9f68c69cc13414e1fffa8d2c917f7b3061c
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-10-06 15:27:08 -0400

    [ARCH] rails new intermodal-example -d postgresql -S -J -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntermodalExample::Application.config.secret_token = 'baf472402b846a5c010d24bd13811ad0b4fe3c91f5e8e709b6aace9deb02831b94e099b7f209ff38cef8878a5fdc742c3d6cab5f9835e302d89f09b2be53564d'
