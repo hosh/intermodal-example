5017554b8f3d1b7674a552e15f63053e074243f7
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-10-11 17:26:44 -0400

    [MODELS] Thing should validate presence of :name

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,4 @@
 class Thing < ActiveRecord::Base
+  # Validations
+  validates_presence_of :name
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'remarkable/active_record'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -15,7 +16,7 @@ RSpec.configure do |config|
 
   # If you'd prefer not to run each of your examples within a transaction,
   # uncomment the following line.
-  config.use_transactional_examples false
+  #config.use_transactional_examples false
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
