07db6aac78f836acfb03e806c3211ef1bb9a3d47
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-10-11 16:37:44 -0400

    [ARCH] Regenerated files from Rails 3.1.1

--- Gemfile
+++ Gemfile
@@ -16,10 +16,12 @@ group :development do
   gem 'pry'
 end
 
+group :development, :test do
+  gem 'rspec-rails', '~> 2.6'
+end
+
 # Bundle gems for certain environments:
 group :test do
-   #gem 'rspec'
-   gem 'rspec-rails'
    gem 'database_cleaner'
    gem 'remarkable', '>=4.0.0.alpha4'
    gem 'remarkable_activemodel', '>=4.0.0.alpha4'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-IntermodalExample::Application.config.secret_token = 'baf472402b846a5c010d24bd13811ad0b4fe3c91f5e8e709b6aace9deb02831b94e099b7f209ff38cef8878a5fdc742c3d6cab5f9835e302d89f09b2be53564d'
+IntermodalExample::Application.config.secret_token = '3dd2d1c06625fe8f483fd366b65987af4e25a92e5ce54b38efabe3961ba07d55fd3a6406d70e446b6d7d57e563ea84d90458f908ef28524ec6ebf19752f6b738'
