da4b84bd3a43bce7e6e23c190af8207575c0ca32
Ho-Sheng Hsiao <hosh@sparkfly.com>
2011-10-12 00:14:00 -0400

    [RAILS] Override default Rails middleware stack

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,25 @@ module IntermodalExample
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    def default_middleware_stack
+      ActionDispatch::MiddlewareStack.new.tap do |middleware|
+        #middleware.use ::Rack::Lock unless config.allow_concurrency
+        middleware.use ::Rack::Runtime
+        middleware.use ::Rails::Rack::Logger
+        middleware.use ::ActionDispatch::RemoteIp, config.action_dispatch.ip_spoofing_check, config.action_dispatch.trusted_proxies
+        middleware.use ::Rack::Sendfile, config.action_dispatch.x_sendfile_header
+        middleware.use ::ActionDispatch::Callbacks, !config.cache_classes
+
+        if config.session_store
+          middleware.use config.session_store, config.session_options
+          middleware.use ::ActionDispatch::Flash
+        end
+
+        #middleware.use ::ActionDispatch::ParamsParser
+        middleware.use ::Rack::MethodOverride
+        middleware.use ::ActionDispatch::Head
+      end
+    end
+
   end
 end
